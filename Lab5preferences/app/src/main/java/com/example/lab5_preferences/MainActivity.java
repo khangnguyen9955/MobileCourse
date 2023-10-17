a7a07e227baa168c938e50ecaad1799ffe7ad308
Nguyen Minh Khang <38356205+khangnguyen9955@users.noreply.github.com>
2023-10-17 23:23:47 +0700

    update

--- Lab5preferences/app/src/main/java/com/example/lab5_preferences/MainActivity.java
+++ Lab5preferences/app/src/main/java/com/example/lab5_preferences/MainActivity.java
@@ -11,4 +11,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    @Override
+    public boolean onCreateOptionsMenu(android.view.Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 }
